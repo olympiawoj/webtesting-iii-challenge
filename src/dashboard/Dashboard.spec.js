93fe6e0179b362f5f51606b47bedb5d401e1389f
olympia <owojcik7@gmail.com>
2019-04-10 16:57:34 -0400

    Create tests for Dasboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,28 @@
 // Test away
+import React from "react";
+import { render } from "react-testing-library";
+import Dashboard from "./Dashboard.js";
+import Display from "../display/display.js";
+import Controls from "../controls/Controls.js";
+
+// Does it render without failure?
+// Does it render correctly / the right thing(s)?
+// Does it render expect output based on props/state/default state?
+// Does it handle events correctly?
+
+describe("<Dashboard/>", () => {
+  // Does it render without failure?
+  it("renders without failure", () => {
+    render(<Dashboard />);
+  });
+
+  //Does it render correctly/the right things?
+
+  it("renders the controls without failure", () => {
+    render(<Display />);
+  });
+
+  it("renders the display without failure", () => {
+    render(<Controls />);
+  });
+});
