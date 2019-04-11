94bc18c82af02c9e0d41a73626352437ce59b8f6
olympia <owojcik7@gmail.com>
2019-04-11 16:19:32 -0400

    Add mock functions to Controls.spec to test if onclick triggers function calls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -4,6 +4,7 @@ import { render, cleanup } from "react-testing-library";
 import Controls from "./Controls";
 import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each"; //add this line to our test file
+import { fireEvent } from "react-testing-library/dist";
 
 describe("Controls", () => {
   it("renders the Controls component without failure", () => {
@@ -36,6 +37,24 @@ describe("Controls", () => {
 
     expect(getByText(/open gate/i)).toBeDisabled();
   });
+
+  it("should call mock function 1x when 'lock gate' is pressed and onClick is triggered", () => {
+    const mock = jest.fn();
+    const { getByText } = render(
+      <Controls closed={true} toggleLocked={mock} />
+    );
+    fireEvent.click(getByText(/lock gate/i));
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call mock function 1x when 'open gate' is pressed and onClick is triggered", () => {
+    const mock = jest.fn();
+    const { getByText } = render(
+      <Controls closed={true} toggleClosed={mock} />
+    );
+    fireEvent.click(getByText(/open gate/i));
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
 });
 
 // describe('Lock/Unlock Button', () => {
@@ -49,3 +68,22 @@ describe("Controls", () => {
 //       expect(mock).toHaveBeenCalledTimes(1);
 //     });
 //   });
+
+/* 
+From the perspective of the Controls component in isolation, we only care about 2 things:
+1. that the button text changes according to the props
+2. that the buttons are enabled/disabled according to the props
+For 1, we can test just as we did for the Display: pass in props and check the button text.
+
+For 2, we finally have a use for *mocks*. Let's start with the basics: 
+----If a button is *enabled*, then its onClick *will trigger* when the button is clicked. 
+----If the button is *disabled*, then its onClick *will not trigger* when the button is clicked. 
+But how would we know if the onClick is triggered if the function is stored in Dashboard (and is thus not available when testing Controls)?
+
+onClick={toggleLocked} where toggleLocked is from dashboard
+
+In order to test this, we must pass in a *mock* function as a prop, then check whether that function was triggered when the button was clicked. 
+
+
+
+*/
